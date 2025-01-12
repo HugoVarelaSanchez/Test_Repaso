38da5fbd56f10aefb034c0f81f62d88e7b265ba6
Varela <158284028+HugoVarelaSanchez@users.noreply.github.com>
2025-01-12 18:38:44 +0100

    Solucionado final calculo nota final

--- Test.py
+++ Test.py
@@ -97,7 +97,7 @@ while True:
 
         
         #Calcular aqui la nota final
-        nota = (  (jugador.acertadas - jugador.falladas / 2) / len(preguntas) )*10
+        nota = (  (jugador.acertadas - jugador.falladas / 2) / longitud_test )*10
         nota = round(nota, 2)
       
         if nota <0:
